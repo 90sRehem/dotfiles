0748c6943f623aefaac5f4fa949c3c7971a5a9e3
Jonathan Rehem <jonathan.rehem@outlook.com>
2025-09-17 12:36:01 -0300

    update consig

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -6,8 +6,5 @@ return {
     suggestion = {
       auto_trigger = true,
     },
-    server_opts_overrides = {
-      cmd = { "/home/rehem/.local/share/mise/installs/node/22.17.1/bin/node", "/home/rehem/.local/share/nvim/lazy/copilot.lua/copilot/js/language-server.js", "--stdio" },
-    },
   },
 }
